7348c727c88f354e59c65b0228ce302764429cdc
ChillyKobold <chillypeacock@gmail.com>
2024-10-18 15:21:07 -0400

    Partial Sanity and Tickets
    Partially implemented tickets and sanity mechanics

--- fall game jam/Scary Gun Game/playermanager.gd
+++ fall game jam/Scary Gun Game/playermanager.gd
@@ -3,14 +3,15 @@ extends Node2D
 
 var score = 0
 var lives = 100
+var combo = 0
 
 func _ready():
-	$ScoreLabel.text = "Score: %d" % score
+	$ScoreLabel.text = "Tickets: %d" % score
 	$LivesLabel.text = "Sanity: %d" % lives
 
 func add_points(points):
 	score += points
-	$ScoreLabel.text = "Score: %d" % score
+	$ScoreLabel.text = "Tickets: %d" % score
 
 func change_lives(lives_mod):
 	lives += lives_mod
--- fall game jam/Scary Gun Game/testingground.gd
+++ fall game jam/Scary Gun Game/testingground.gd
@@ -1,19 +1,9 @@
 extends Node2D
 
-var heights = [100, 175, 250, 325, 400]
-var speed = 200  # enemy speed
 
 func _on_timer_timeout():
-	var screen_width = int(get_viewport_rect().size.x)  # Cast screen_width to int
-	var screen_height = int(get_viewport_rect().size.y)  # Cast screen_height to int
+	get_tree().call_group("score", "change_lives", -1)
 	var bad_guy_scene = preload("res://badguy.tscn")
 	var bad_guy = bad_guy_scene.instantiate()
-
-	# Pick a random spawn row/column
-	var random_height = heights[randi() % heights.size()]
-
-	# Assign position based on movement type, which will be decided in badguy.gd
-	bad_guy.position = Vector2(0, random_height)
-
 	add_child(bad_guy)
-	print("Bad guy spawned at: %s" % [bad_guy.position])
+	print("Bad guy spawned at: %s with movement type %s" % [bad_guy.position, bad_guy.movement_type])
