ac5c478969f4b80c37af5ddebe22ce3293539950
Racc000n <v009abd@gmail.com>
2024-10-19 11:53:28 -0400

    Shopkeeps eyes animtaed!!

---
+++ fall game jam/Scary Gun Game/Assets/shop.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+@onready var eyesMoves = $EyeMoves 
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	eyesMoves.play("ShopEyes")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
