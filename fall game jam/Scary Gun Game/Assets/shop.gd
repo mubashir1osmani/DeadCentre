49f9aa53a298ab42d1190924b26ceda22826f111
Racc000n <v009abd@gmail.com>
2024-10-19 17:40:12 -0400

    ShoppStuffAgain

--- fall game jam/Scary Gun Game/Assets/shop.gd
+++ fall game jam/Scary Gun Game/Assets/shop.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-@onready var eyesMoves = $EyeMoves 
+@onready var eyesMoves = $ShopKeep 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	eyesMoves.play("ShopEyes")
