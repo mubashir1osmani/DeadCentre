aaa551d32c683fa6864397edb9252056e6cd3142
Racc000n <v009abd@gmail.com>
2024-10-19 18:26:55 -0400

    redid my inven code works now bitchess

--- fall game jam/Scary Gun Game/Inventory/inventory_ui.gd
+++ fall game jam/Scary Gun Game/Inventory/inventory_ui.gd
@@ -1,10 +1,32 @@
 extends Control
 
+@onready var inv: Inv = preload("res://Inventory/playerinv.tres")
+
 var is_open = true 
 
 func _ready():
 	open()
+	$"3dGlassess".visible = false
+	$"Candy".visible = false
+	$"IceCube".visible = false
+	$"Magazine".visible = false
+	$"Pillow".visible = false
+	$"Pills".visible = false
 	
+	
+func _process(delta):
+	if inv.items[0].bought :
+		$"3dGlassess".visible = true
+	if inv.items[1].bought :
+		$"Candy".visible = true
+	if inv.items[2].bought :
+		$"IceCube".visible = true
+	if inv.items[3].bought :
+		$"Magazine".visible = true
+	if inv.items[4].bought :
+		$"Pillow".visible = true
+	if inv.items[5].bought :
+		$"Pills".visible = true
 
 func open():
 	visible = true
