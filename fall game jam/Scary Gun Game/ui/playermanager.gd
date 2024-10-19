d654d976b7b1a379d0527381b2934626be9dcfa3
Racc000n <v009abd@gmail.com>
2024-10-19 18:11:06 -0400

    ResizingItemsInInven

---
+++ fall game jam/Scary Gun Game/ui/playermanager.gd
@@ -0,0 +1,23 @@
+#playermanager.gd
+extends Node2D
+
+@export var inv = Inv
+
+var score = 0
+var lives = 100
+
+func _ready():
+	$ScoreLabel.text = "Score: %d" % score
+	$LivesLabel.text = "Sanity: %d" % lives
+
+func add_points(points):
+	score += points
+	$ScoreLabel.text = "Score: %d" % score
+
+func change_lives(lives_mod):
+	lives += lives_mod
+	$LivesLabel.text = "Sanity: %d" % lives
+
+func death():
+	if lives == 0:
+		get_tree().change_scene_to_file("res://gameover.tscn")
