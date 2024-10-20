a41a6290640842e1e1da786624d6b412c52c9217
Racc000n <v009abd@gmail.com>
2024-10-19 20:51:00 -0400

    pew pewfor round 1

--- fall game jam/Scary Gun Game/testingground.gd
+++ fall game jam/Scary Gun Game/testingground.gd
@@ -74,4 +74,4 @@ func _on_game_start_timer_timeout():
 	
 func _input(event):
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-		pass #PUT THE SFX HERE
+		$gunshot.play()
